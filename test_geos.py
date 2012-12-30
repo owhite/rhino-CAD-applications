3a4c92174601140933732cc378bf67d0db28493e
smouldering.dog <smouldering.dog@9ab31d79-23fe-9de5-7290-c30355ea2eae>
2012-12-30 14:19:06 +0000

    refined test_geos.

--- test_geos.py
+++ test_geos.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
+from shapely.geometry import LineString
+from shapely.geometry import Point
 from shapely.geos import lgeos
 
-print "hoping for: (1, 6, 2)\nand: 3.2.2-CAPI-1.6.2\n\nversions:\n"
+print "hoping for a minimum of: (1, 6, 2)\nand: 3.2.2-CAPI-1.6.2\n\nversions:\n"
 
 print lgeos.geos_capi_version
 print lgeos._lgeos.GEOSversion()
+
+print '\ntesting interpolate() and project()'
+
+ip = LineString([(0, 0), (0, 1), (1, 1)]).interpolate(1.5)
+x = LineString([(0, 0), (0, 1), (1, 1)]).project(ip)
+
+if x == 1.5:
+    print "looks like it's working"
+else:
+    print 'fail'
